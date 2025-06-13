76166bb2b433adfc36e151afe1f698ca9a02b1cc
el-bethel-trust <issacrajebl@gmail.com>
2025-06-13 20:15:12 +0530

    feat: bulk lock inplace limiation with batching

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import { sentry } from "@hono/sentry";
 import { trimTrailingSlash } from "hono/trailing-slash";
 import { ivr, unlock, admin, dailyVerses } from "./routes";
 import { log } from "./utils";
-import scheduled from "./cron";
+import scheduled, { postHolyPeriodTask } from "./cron";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 app.use("*", sentry());
@@ -13,10 +13,10 @@ app.use("*", cors()); // TODO: need to do cors properly during staging
 app.use(logger());
 app.use(trimTrailingSlash());
 app.onError((err, c) => {
-	log.error("Uncaught Error:", err);
-	c.get("sentry").setTag("domain", "backend");
-	c.get("sentry").captureException(err);
-	return c.json({ error: "Internal Server Error", message: err.message }, 500);
+  log.error("Uncaught Error:", err);
+  c.get("sentry").setTag("domain", "backend");
+  c.get("sentry").captureException(err);
+  return c.json({ error: "Internal Server Error", message: err.message }, 500);
 });
 
 app.route("/ivr", ivr);
@@ -25,10 +25,14 @@ app.route("/admin", admin);
 app.route("/daily-verses", dailyVerses);
 
 app.get("/", async (c) => {
-	return c.text("Hello El-Bethel!");
+  return c.text("Hello El-Bethel!");
+});
+app.get("/lock", async (c) => {
+  await postHolyPeriodTask();
+  return c.text("ran well!!");
 });
 
 export default {
-	fetch: app.fetch,
-	scheduled,
+  fetch: app.fetch,
+  scheduled,
 };
