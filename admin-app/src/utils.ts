bd8d821135a0b6dfcafe50689edf60c78484ff5c
el-bethel-trust <issacrajebl@gmail.com>
2025-06-13 09:21:25 +0530

    feat: toast for web

--- admin-app/src/utils.ts
+++ admin-app/src/utils.ts
@@ -1,8 +1,13 @@
 import { Toast } from '@capacitor/toast';
+import { Capacitor } from '@capacitor/core';
+//@ts-ignore
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';
 
 export const showToast = async (message: string) => {
-  await Toast.show({
-    text: message,
-    duration: 'long',
-  });
+  if (Capacitor.isNativePlatform()) {
+    await Toast.show({ text: message, duration: 'long' });
+  } else {
+    toastr.info(message, { timeout: 3500 });
+  }
 };
